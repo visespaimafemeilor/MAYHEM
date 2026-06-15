29dd9ee9272ee43843faf5a5ac0f53b93c7635e4
Laura <113705334+visespaimafemeilor@users.noreply.github.com>
2026-06-15 17:52:41 +0300

    rezolare buguri

--- tumblr-clone-laravel/routes/web.php
+++ tumblr-clone-laravel/routes/web.php
@@ -19,24 +19,27 @@ Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/create', [PostController::class, 'create']);
-Route::get('/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::post('/edit', [PostController::class, 'edit']);
-Route::get('/delete', [PostController::class, 'delete'])->name('posts.delete');
+Route::middleware('auth')->group(function () {
+    Route::get('/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/create', [PostController::class, 'create']);
+    Route::get('/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::post('/edit', [PostController::class, 'edit']);
+    Route::get('/delete', [PostController::class, 'delete'])->name('posts.delete');
 
-Route::post('/reblog', [PostController::class, 'reblog'])->name('posts.reblog');
+    Route::post('/reblog', [PostController::class, 'reblog'])->name('posts.reblog');
+
+    Route::post('/follow', [ProfileController::class, 'follow'])->name('follow');
+
+    Route::get('/settings', [SettingsController::class, 'index'])->name('settings');
+    Route::post('/settings', [SettingsController::class, 'index']);
+    Route::post('/settings/avatar', [SettingsController::class, 'updateAvatar'])->name('settings.avatar');
+    Route::post('/settings/password', [SettingsController::class, 'updatePassword'])->name('settings.password');
+
+    Route::post('/like', [PostController::class, 'like'])->name('like');
+
+    Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications');
+    Route::post('/notifications/read', [NotificationController::class, 'markAsRead'])->name('notifications.read');
+    Route::get('/notifications/count', [NotificationController::class, 'unreadCount'])->name('notifications.count');
+});
 
 Route::get('/profile/{username}', [ProfileController::class, 'index'])->name('profile');
-Route::post('/follow', [ProfileController::class, 'follow'])->name('follow');
-
-Route::get('/settings', [SettingsController::class, 'index'])->name('settings');
-Route::post('/settings', [SettingsController::class, 'index']);
-Route::post('/settings/avatar', [SettingsController::class, 'updateAvatar'])->name('settings.avatar');
-Route::post('/settings/password', [SettingsController::class, 'updatePassword'])->name('settings.password');
-
-Route::post('/like', [PostController::class, 'like'])->name('like');
-
-Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications');
-Route::post('/notifications/read', [NotificationController::class, 'markAsRead'])->name('notifications.read');
-Route::get('/notifications/count', [NotificationController::class, 'unreadCount'])->name('notifications.count');
